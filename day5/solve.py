1425384aa4d99fa884d1c855e9d6863816bbfeec
Leonardo Dos Santos Maia <ledos-sa@c1r15s15.42porto.com>
2023-11-27 22:59:11 +0000

    mais uma moedinha mais uma voltinha

--- day5/solve.py
+++ day5/solve.py
@@ -1,26 +1,21 @@
-
-
 datafile = "data.txt"
-datafile = "test.txt"
-# data = [[0 for j in range(8)] for i in range(10)]
-data = [[] for _ in range(4)]
+# datafile = "test.txt"
+data = [[] for _ in range(10)]
 instructions = []
-
 with open(datafile) as file:
   for index, line in enumerate(file):
     if line[:4] != 'move':
       for i, c in enumerate(line):
         if c.isalpha():
-          print(c, i//4 + 1)
           data[i//4 + 1].append(c)
     else:
-      ins = line.split()
-      instructions.append((int(ins[1]), int(ins[3]), int(ins[5])))
+      instructions.append([int(ins) for i, ins in enumerate(line.split()) if i % 2 !=0])
 
-for ins in instructions:
-  x = data[ins[1]][:ins[0]]
-  data[ins[1]] = data[ins[1]][ins[0] - 1:]
-  data[ins[2]] = x + data[ins[2]]
-  print(data)
+for i, ins in enumerate(instructions):
+  quantity, src, dst = ins[0], ins[1], ins[2]
+  x = data[src][:quantity][::-1]
+  data[src] = data[src][quantity:]
+  data[dst] = x + data[dst]
 
-
+for d in data[1:]:
+  print(d[0])
