d49d8e680f491f8df5e1807781da6bb2c17c6676
Leonardo Dos Santos Maia <ledos-sa@c1r14s7.42porto.com>
2023-11-28 18:59:28 +0000

    day 5

--- day5/solve.py
+++ day5/solve.py
@@ -1,3 +1,28 @@
+# datafile = "data.txt"
+# # datafile = "test.txt"
+# data = [[] for _ in range(10)]
+# instructions = []
+# with open(datafile) as file:
+#   for index, line in enumerate(file):
+#     if line[:4] != 'move':
+#       for i, c in enumerate(line):
+#         if c.isalpha():
+#           data[i//4 + 1].append(c)
+#     else:
+#       instructions.append([int(ins) for i, ins in enumerate(line.split()) if i % 2 !=0])
+#
+# for i, ins in enumerate(instructions):
+#   quantity, src, dst = ins[0], ins[1], ins[2]
+#   x = data[src][:quantity][::-1]
+#   data[src] = data[src][quantity:]
+#   data[dst] = x + data[dst]
+#
+# for d in data[1:]:
+#   print(d[0])
+
+
+
+#part 2
 datafile = "data.txt"
 # datafile = "test.txt"
 data = [[] for _ in range(10)]
@@ -13,7 +38,7 @@ with open(datafile) as file:
 
 for i, ins in enumerate(instructions):
   quantity, src, dst = ins[0], ins[1], ins[2]
-  x = data[src][:quantity][::-1]
+  x = data[src][:quantity]
   data[src] = data[src][quantity:]
   data[dst] = x + data[dst]
 
